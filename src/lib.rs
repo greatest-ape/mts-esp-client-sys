c0e20ebcc2068d064420206810c9233a6abfe377
Joakim Frostegård <joakim.frostegard@gmail.com>
2024-03-17 23:44:11 +0100

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,7 @@
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+
+//! (MTS-ESP)[https://github.com/ODDSound/MTS-ESP/tree/main] client bindings
+
+include!(concat!(env!("OUT_DIR"), "/bindings.rs"));

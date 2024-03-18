da259c328e664d21b657311af827111fb934fff2
Joakim Frostegård <joakim.frostegard@gmail.com>
2024-03-18 17:40:24 +0100

    Remove bindgen and write bindings in lib.rs instead

--- build.rs
+++ build.rs
@@ -1,31 +1,7 @@
-use std::path::PathBuf;
-
 fn main() {
     cc::Build::new()
         .cpp(true)
         .file("include/libMTSClient.hpp")
         .file("include/libMTSClient.cpp")
         .compile("libMTSClient");
-
-    // The bindgen::Builder is the main entry point
-    // to bindgen, and lets you build up options for
-    // the resulting bindings.
-    let bindings = bindgen::Builder::default()
-        // The input header we would like to generate
-        // bindings for.
-        .header("include/libMTSClient.hpp")
-        .allowlist_item("MTS_.*")
-        // Tell cargo to invalidate the built crate whenever any of the
-        // included header files changed.
-        .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
-        // Finish the builder and generate the bindings.
-        .generate()
-        // Unwrap the Result and panic on failure.
-        .expect("Unable to generate bindings");
-
-    // Write the bindings to the $OUT_DIR/bindings.rs file.
-    let out_path = PathBuf::from(std::env::var("OUT_DIR").unwrap());
-    bindings
-        .write_to_file(out_path.join("bindings.rs"))
-        .expect("Couldn't write bindings!");
 }
--- src/lib.rs
+++ src/lib.rs
@@ -4,4 +4,80 @@
 
 //! (MTS-ESP)[https://github.com/ODDSound/MTS-ESP/tree/main] client bindings
 
-include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+/// Opaque datatype for MTSClient.
+#[repr(C)]
+pub struct MTSClient {
+    _unused: [u8; 0],
+    _marker: core::marker::PhantomData<(*mut u8, core::marker::PhantomPinned)>,
+}
+
+extern "C" {
+    /// Register as a client. Call from the plugin constructor.
+    pub fn MTS_RegisterClient() -> *mut MTSClient;
+    /// Deregister as a client. Call from the plugin destructor.
+    pub fn MTS_DeregisterClient(client: *mut MTSClient);
+
+    /// Check if the client is currently connected to a master plugin.
+    pub fn MTS_HasMaster(client: *mut MTSClient) -> bool;
+
+    /// Returns true if note should not be played. MIDI channel argument should be included if possible (0-15), else set to -1.
+    pub fn MTS_ShouldFilterNote(
+        client: *mut MTSClient,
+        midinote: ::std::os::raw::c_char,
+        midichannel: ::std::os::raw::c_char,
+    ) -> bool;
+
+    /// Retuning a midi note. Pick the version that makes your life easiest! MIDI channel argument should be included if possible (0-15), else set to -1.
+    pub fn MTS_NoteToFrequency(
+        client: *mut MTSClient,
+        midinote: ::std::os::raw::c_char,
+        midichannel: ::std::os::raw::c_char,
+    ) -> f64;
+    /// Retuning a midi note. Pick the version that makes your life easiest! MIDI channel argument should be included if possible (0-15), else set to -1.
+    pub fn MTS_RetuningInSemitones(
+        client: *mut MTSClient,
+        midinote: ::std::os::raw::c_char,
+        midichannel: ::std::os::raw::c_char,
+    ) -> f64;
+    /// Retuning a midi note. Pick the version that makes your life easiest! MIDI channel argument should be included if possible (0-15), else set to -1.
+    pub fn MTS_RetuningAsRatio(
+        client: *mut MTSClient,
+        midinote: ::std::os::raw::c_char,
+        midichannel: ::std::os::raw::c_char,
+    ) -> f64;
+
+    /// MTS_FrequencyToNote() is a helper function returning the note number whose pitch is closest to the supplied frequency. Two versions are provided:
+    /// The first is for the simplest case: supply a frequency and get a note number back.
+    /// If you intend to use the returned note number to generate a note-on message on a specific, pre-determined MIDI channel, set the midichannel argument to the destination channel (0-15), else set to -1.
+    /// If a MIDI channel is supplied, the corresponding multi-channel tuning table will be queried if in use, else multi-channel tables are ignored.
+    pub fn MTS_FrequencyToNote(
+        client: *mut MTSClient,
+        freq: f64,
+        midichannel: ::std::os::raw::c_char,
+    ) -> ::std::os::raw::c_char;
+
+    /// Use the second version if you intend to use the returned note number to generate a note-on message and where you have the possibility to send it on any MIDI channel.
+    /// The midichannel argument is a pointer to a char which will receive the MIDI channel on which the note message should be sent (0-15).
+    /// Multi-channel tuning tables are queried if in use.
+    pub fn MTS_FrequencyToNoteAndChannel(
+        client: *mut MTSClient,
+        freq: f64,
+        midichannel: *mut ::std::os::raw::c_char,
+    ) -> ::std::os::raw::c_char;
+
+    /// Returns the name of the current scale.
+    pub fn MTS_GetScaleName(client: *mut MTSClient) -> *const ::std::os::raw::c_char;
+
+    /// Parse incoming MIDI data to update local retuning. All formats of MTS sysex message accepted.
+    pub fn MTS_ParseMIDIDataU(
+        client: *mut MTSClient,
+        buffer: *const ::std::os::raw::c_uchar,
+        len: ::std::os::raw::c_int,
+    );
+    /// Parse incoming MIDI data to update local retuning. All formats of MTS sysex message accepted.
+    pub fn MTS_ParseMIDIData(
+        client: *mut MTSClient,
+        buffer: *const ::std::os::raw::c_char,
+        len: ::std::os::raw::c_int,
+    );
+}
